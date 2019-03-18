8cd02573a2a2c63310935b2a60db7e3a1cfe573b
yagisuke <yagisuke.0209@gmail.com>
2019-03-19 00:23:15 +0900

    layout-compoent

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Link from 'next/link'
+import Layout from '../components/Layout'
 
 const Index = () => (
-  <div>
-    <h1>Home</h1>
+  <Layout title="Home">
     <Link href="/about">
       <a>Go to About</a>
     </Link>
     <p>Welcome to the home page.</p>
-  </div>
+  </Layout>
 )
 
 export default Index
