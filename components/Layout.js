99ed44f65e76dedbcf750e1c7742b829d74bbd6e
yagisuke <yagisuke.0209@gmail.com>
2019-03-19 16:34:33 +0900

    nprogress

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,21 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 const Layout = (props) => (
   <div className="root">
+    <Head>
+      <title>Next Portfolio</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+    </Head>
     <header>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
