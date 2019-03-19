d2234e1fb17d98d37a39a8f53dc37995003d100f
yagisuke <yagisuke.0209@gmail.com>
2019-03-19 18:36:08 +0900

    github-api

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,27 @@
 import Layout from '../../components/Layout'
-import Link from 'next/link'
+import { Component } from 'react'
+import fetch from 'isomorphic-unfetch'
 
-export default () => (
-  <Layout title="About">
-    <Link href="/">
-      <a>Go to Home</a>
-    </Link>
-    <p>A Javascript programmer.</p>
-    <img src="/static/avatar.jpg" alt="me" height="200px" />
-  </Layout>
-)
+class About extends Component {
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/yagisuke')
+    const data = await res.json()
+    return { user: data }
+  }
+
+  render() {
+    const { user } = this.props
+    return (
+      <Layout title="About">
+        <p>
+          <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+            {user.bio}
+          </a>
+        </p>
+        <img src={user.avatar_url} alt="me" height="200px" />
+      </Layout>
+    )
+  }
+}
+
+export default About
